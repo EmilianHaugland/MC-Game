2d0e949e71bcdf360b072e4a716b4d4dd2f28b3f
Highacid <pelleV.haugen@gmail.com>
2023-07-03 16:02:21 +0200

    new

--- MC-game_test.py
+++ MC-game_test.py
@@ -55,6 +55,9 @@ wall = pygame.Rect(-800, 0, 100, SCREEN_HEIGHT)
 car1 = pygame.image.load("carh.png")
 car2 = pygame.image.load("car2h.png")
 car3 = pygame.image.load("car3h.png")
+car4 = pygame.image.load("car4h.png")
+car5 = pygame.image.load("car5h.png")
+car6 = pygame.image.load("car6h.png")
 
 motorcycle = pygame.image.load("motorcycle.png")
 
@@ -71,7 +74,7 @@ carcrashY = None
 
 angle = 0
 
-car_list = [car1, car2, car3, trailer]
+car_list = [car1, car2, car3, car4, car5, car6, trailer]
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -83,7 +86,7 @@ ape = random.randint(0,1)
 run = True
 game_state = "start"
 
-color = (255, 255, 255)
+color = (50, 50, 50)
 
 clock = pygame.time.Clock()
 
@@ -132,8 +135,8 @@ def spawn_car():
 
 linegroup = []
 
-evnt, t, trail = pygame.USEREVENT+1, round(300 - (CAR_SPEED * 6)), []
-pygame.time.set_timer(evnt, t)
+evnt = pygame.USEREVENT+1
+pygame.time.set_timer(evnt, 300)
 
 def liness():
     y = 315
@@ -154,8 +157,8 @@ while run:
     clock.tick(90)  # Limit the frame rate to 90 FPS
 
     if Score_value > 1000 and Score_value < 3000 and CAR_SPEED < 10:
-         Score_num = 1.5
-         CAR_SPEED = CAR_SPEED * 1.005
+        Score_num = 1.5
+        CAR_SPEED = CAR_SPEED * 1.005
     elif Score_value > 3000 and Score_value < 6000 and CAR_SPEED < 15:
         Score_num = 2
         CAR_SPEED = CAR_SPEED * 1.005
@@ -172,6 +175,17 @@ while run:
         PLAYER_SPEED = 15 
         CAR_SPEED = CAR_SPEED * 1.005
 
+    if Score_value > 1000 and Score_value < 1010:
+        pygame.time.set_timer(evnt, 250)
+    elif Score_value > 3000 and Score_value < 3010:
+        pygame.time.set_timer(evnt, 200)
+    elif Score_value > 6000 and Score_value < 6010:
+        pygame.time.set_timer(evnt, 100)
+    elif Score_value > 10000 and Score_value < 10010:
+        pygame.time.set_timer(evnt, 20)
+    elif Score_value > 15000 and Score_value < 15010:
+        pygame.time.set_timer(evnt, 5)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
@@ -183,14 +197,14 @@ while run:
                     Time = 0  # Reset the time when the game starts
             elif game_state == "game_over":
                 if event.key == pygame.K_r:
-                    color = (255, 255, 255)
                     # Restart the game
+                    color = (255, 255, 255)
                     game_state = "running"
                     Time = 0  # Reset the time when the game restarts
                     cars.empty()
                     player.y = (SCREEN_HEIGHT - CAR_HEIGHT) // 2
                     Score_value = 1
-                    CAR_SPEED = 5
+                    CAR_SPEED = 10
                     carnumber = 0
                     carcrashX = None
                     carcrashY = None
@@ -205,12 +219,6 @@ while run:
 
     if game_state == "start":
         # Show start menu
-        for line in linegroup:
-            color = (50, 50, 50)
-            pygame.draw.rect(screen, (color), line)
-            line.move_ip(-CAR_SPEED - 7, 0)
-            if line.colliderect(wall):
-                linegroup.remove(line)
         start_text = font.render("Press SPACE to Start", True, (255, 255, 255))
         your_highscore = font.render("Your highscore is " + str(highscore), True, (255, 255, 255))
         screen.blit(your_highscore, (SCREEN_WIDTH // 2 - your_highscore.get_width() // 2, SCREEN_HEIGHT // 2 -50))
@@ -218,10 +226,6 @@ while run:
     elif game_state == "running":
         if Time == 0:
             Time = pygame.time.get_ticks() / 1000
-       
-        # Draw grass
-        pygame.draw.rect(screen, (0, 128, 0), pygame.Rect(0, 0, SCREEN_WIDTH, lane_positions[0]))
-        pygame.draw.rect(screen, (0, 128, 0), pygame.Rect(0, lane_positions[NUM_LANES - 1] + LANE_WIDTH, SCREEN_WIDTH, SCREEN_HEIGHT))
 
         screen.fill((50,50,50))
 
@@ -235,12 +239,6 @@ while run:
         tb_lane_y = lane_positions[NUM_LANES - 5]
         pygame.draw.line(screen, (255, 255, 255), (0, tb_lane_y + LANE_WIDTH - 150), (SCREEN_WIDTH, tb_lane_y + LANE_WIDTH - 150), 4)
 
-        for line in linegroup:
-         pygame.draw.rect(screen, (255, 255, 255), line)
-         line.move_ip(-CAR_SPEED - 7, 0)
-         if line.colliderect(wall):
-            linegroup.remove(line)
-
         # Move and draw each car
         for car in cars:
             car.rect.move_ip(-CAR_SPEED, 0)  # Increase the movement amount to make cars go faster
@@ -248,6 +246,7 @@ while run:
 
             # Check if the car collides with the player
             if car.rect.colliderect(player):
+                color = (50, 50, 50)
                 game_state = "game_over"
                 carcrashX = car.rect.x
                 carcrashY = car.rect.y
@@ -283,6 +282,12 @@ while run:
         elif angle < 0:
             angle = angle + 1 * 2.5
             motorcycle1 = pygame.transform.rotate(motorcycle, angle)
+        
+        for line in linegroup:
+            pygame.draw.rect(screen, (color), line)
+            line.move_ip(-CAR_SPEED - 7, 0)
+            if line.colliderect(wall):
+                linegroup.remove(line)
 
         screen.blit(motorcycle1, (player.x - int(motorcycle1.get_width() / 2 - 20), player.y - int(motorcycle1.get_height() / 2 - 10)))
 
@@ -290,19 +295,13 @@ while run:
 
         show_score(textX, textY)
 
-        Score_value += Score_num
+        Score_value += Score_num + 1
 
         with open("data.txt", "r") as f:
             highscore = f.read(99999)
 
     elif game_state == "game_over":
         # Show game over menu
-        for line in linegroup:
-            color = (50, 50, 50)
-            pygame.draw.rect(screen, (color), line)
-            line.move_ip(-CAR_SPEED - 7, 0)
-            if line.colliderect(wall):
-                linegroup.remove(line)
         game_over_text = font.render("Game Over", True, (255, 255, 255))
         restart_text = font.render("Press R to Restart", True, (255, 255, 255))
         quit_text = font.render("Press Q to Quit", True, (255, 255, 255))
@@ -310,14 +309,14 @@ while run:
         new_highscore_text = font.render("You got the new highscore of " + str(round(Score_value)), True, (255, 255, 255))
         your_highscore = font.render("Your highscore is " + highscore, True, (255, 255, 255))
         if Score_value < int(highscore):
-            screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - 150))
-            screen.blit(your_highscore, (SCREEN_WIDTH // 2 - your_highscore.get_width() // 2, SCREEN_HEIGHT // 2 -100))
-            screen.blit(score_text, (SCREEN_WIDTH // 2 - score_text.get_width() // 2, SCREEN_HEIGHT // 2 - 50))
+            screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - 160))
+            screen.blit(your_highscore, (SCREEN_WIDTH // 2 - your_highscore.get_width() // 2, SCREEN_HEIGHT // 2 -110))
+            screen.blit(score_text, (SCREEN_WIDTH // 2 - score_text.get_width() // 2, SCREEN_HEIGHT // 2 - 60))
         if Score_value > int(highscore):
-            screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - 100))
-            screen.blit(new_highscore_text, (SCREEN_WIDTH // 2 - new_highscore_text.get_width() // 2, SCREEN_HEIGHT // 2 - 50))
-        screen.blit(restart_text, (SCREEN_WIDTH // 2 - restart_text.get_width() // 2, SCREEN_HEIGHT // 2))
-        screen.blit(quit_text, (SCREEN_WIDTH // 2 - quit_text.get_width() // 2, SCREEN_HEIGHT // 2 + 50))
+            screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - 110))
+            screen.blit(new_highscore_text, (SCREEN_WIDTH // 2 - new_highscore_text.get_width() // 2, SCREEN_HEIGHT // 2 - 60))
+        screen.blit(restart_text, (SCREEN_WIDTH // 2 - restart_text.get_width() // 2, SCREEN_HEIGHT // 2 - 5))
+        screen.blit(quit_text, (SCREEN_WIDTH // 2 - quit_text.get_width() // 2, SCREEN_HEIGHT // 2 + 40))
 
     pygame.display.update()
 
